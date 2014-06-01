019b4050fc84b5fa7bedab82aeb2d6028e02bd12
adrian_ortuzar <adrianortuzar@gmail.com>
2014-06-01 11:42:08 +0200

    start commit

---
+++ spec/UserSpec.js
@@ -0,0 +1,109 @@
+describe("Class User test", function() {
+	function getUserWithData(){
+		var user = new User({
+			password: "1234",
+			email: "adrianortuzar@gmail.com",
+			name:"Adrian",
+			surname:"Ortuzar",
+			telephoneNumber : 123456
+		});
+		return user;
+	}
+
+	describe("Change properties with the change method", function() {
+
+		var user = getUserWithData();
+
+		user.change('name', 'Juan', "1234");
+		it("user.name Should be a string 'Juan'", function() {
+			expect(user.name).toBe("Juan");
+		});
+
+		user.change('surname', 'Rodrigo', "1234");
+		it("user.surname Should be a 'Rodrigo'", function() {
+			expect(user.surname).toBe("Rodrigo");
+		});
+
+		it("user.fullname Should be a 'Juan Rodrigo'", function() {
+			expect(user.fullname).toBe("Juan Rodrigo");
+		});
+
+		user.change('telephoneNumber', 66666, "1234");
+		it("user.telephoneNumber Should be 66666", function() {
+			expect(user.telephoneNumber).toBe(66666);
+		});
+	});
+
+	describe("Change properties with out password", function() {
+		var user = getUserWithData();
+
+		user.name = "Juan";
+		it("user.name Should be a string 'Adrian'", function() {
+			expect(user.name).toBe("Adrian");
+		});
+
+		user.surname = "Rodrigo";
+		it("user.surname Should be a 'Ortuzar'", function() {
+			expect(user.surname).toBe("Ortuzar");
+		});
+
+		user.fullname = "Juan Rodrigo";
+		it("user.fullname Should be a 'Adrian Ortuzar'", function() {
+			expect(user.fullname).toBe("Adrian Ortuzar");
+		});
+
+		user.telephoneNumber = 12344;
+		it("user.telephoneNumber Should be 123456", function() {
+			expect(user.telephoneNumber).toBe(123456);
+		});
+	});
+
+	describe("Initialize with real data", function() {
+		var user = getUserWithData();
+
+		it("user.name Should be a string 'Adrian'", function() {
+			expect(user.name).toBe("Adrian");
+		});
+
+		it("user.surname Should be a 'Ortuzar'", function() {
+			expect(user.surname).toBe("Ortuzar");
+		});
+
+		it("user.fullname Should be a 'Adrian Ortuzar'", function() {
+			expect(user.fullname).toBe("Adrian Ortuzar");
+		});
+
+		it("user.telephoneNumber Should be 123456", function() {
+			expect(user.telephoneNumber).toBe(123456);
+		});
+	});
+
+	describe("Initialize with minimun value", function() {	
+		var user = new User({
+			email: "adrianortuzar@gmail.com",
+			password :"1234"
+		});
+
+		it("user Should have a email", function() {
+			expect(typeof user.email).toBe("string");
+		});
+
+		it("user.name Should be a string", function() {
+			expect(typeof user.name).toBe("string");
+		});
+
+		it("user.surname Should be a string", function() {
+			expect(typeof user.surname).toBe("string");
+		});
+
+		it("user.fullname Should be a string", function() {
+			expect(typeof user.fullname).toBe("string");
+		});
+
+		it("user.telephoneNumber Should be a number", function() {
+			expect(typeof user.telephoneNumber).toBe("number");
+		});
+	});
+});
+
+
---
+++ src/User.js
@@ -0,0 +1,102 @@
+(function(){
+    "use strict";
+    //private
+    var _password;
+
+    //public
+    var User = function(options){
+    	minimunData.call(this, options);
+
+    	_password = options.password;
+
+    	this.lastPasswordValue = _password;
+
+    	this.name = (options && options.name && (typeof options.surname === "string")) ? options.name : "";
+
+    	this.surname = (options && options.surname && (typeof options.surname === "string")) ? options.surname : "";
+
+        this.telephoneNumber = (options && options.telephoneNumber && (typeof options.telephoneNumber === "number")) ? options.telephoneNumber : 0;
+
+        this.lastPasswordValue = null;
+    };
+    
+    //make user global variable
+    window.User = User;
+
+    //define user prototype
+    User.prototype = {
+    	//name
+        get name(){
+            return this._name;
+        },
+        set name(val){
+        	if (this.lastPasswordValue === _password) {
+        		this._name = val;
+        	}
+        	else{
+        		console.warn("Set name with the user property change");
+        	}
+        },
+        //surname
+        get surname(){
+            return this._surname;
+        },
+        set surname(val){
+        	if (this.lastPasswordValue === _password) {
+        		this._surname = val;
+        	}
+        	else{
+        		console.warn("Set surname with the user property change");
+        	}
+        },
+        //fullname
+        get fullname(){
+    		return this.name+" "+this.surname;
+    	},
+    	//telephoneNumber
+        get telephoneNumber(){
+            return this._telephoneNumber;
+        },
+        set telephoneNumber(val){
+        	if (this.lastPasswordValue === _password) {
+        		this._telephoneNumber = val;
+        	}
+        	else{
+        		console.warn("Set telephoneNumber with the user property change");
+        	}
+        },
+    };
+
+    /**
+     * [change public User method to change properties of User like name, surname, telephone providing a password. If the password is correct the property changed if not will print a warning in the browser console.]
+     * @param  {[string]} key           [User property name]
+     * @param  {[string]} value         [User value for the providing property]
+     * @param  {[type]} passwordValue [password of the user]
+     * @return {[value]}               [return the value providing if the property change if not will print a console warning.]
+     */
+    User.prototype.change = function(key, value, passwordValue){
+    	if (_password === passwordValue) {
+    		this.lastPasswordValue = passwordValue;
+    		this[key] = value;
+    		this.lastPasswordValue = null;
+    		return value;	
+    	}
+    	else {
+    		console.warn("Trying change "+key+" with a wrong password");
+    	}	
+    };
+
+    /**
+     * [minimunData private function that is going to determine which is the minimum data to construct a User. If the data is not valid is going to throw a exception.]
+     * @param  {[object]} options [options of User]
+     */
+    function minimunData (options){
+    	if (options && options.password && options.email) {
+    		this.email = options.email;
+    	}
+    	else {
+    		throw "User should have a minimun data, password and email";
+    	}
+    }
+
+})();
